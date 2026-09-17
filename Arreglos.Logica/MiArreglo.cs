f8d83d1d61738c612c5f6619eb0fe9f991e5c591
lupitaor <amartinez@itesa.edu.mx>
2026-09-17 07:28:01 -0600

    Versión_02: Método llenar arreglo.

---
+++ Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,52 @@
+﻿namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //Campos o atributos
+        private int _tope;
+        private int[] _arreglo;
+
+        //Constructor
+        public MiArreglo(int n)
+        {
+            N = n;
+            _arreglo = new int[n];
+            _tope = 0;
+        }
+        //Propiedades
+        public int N { get; }
+        public bool EstaLleno => _tope == N;
+        public bool EstaVacio => _tope == 0;
+
+        //Métodos
+        public void Llenar(int minimo, int maximo)
+        {
+            Random random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = random.Next(minimo, maximo);
+
+            }
+            _tope = N;
+
+        }
+
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                Console.WriteLine("El arreglo esta vacío");
+            }
+
+            string salida = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                salida += $"{_arreglo[i]}\t";
+            }
+
+            return salida;
+        }
+
+
+    }
+}
--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Arreglos");
+
+
+
+
+MiArreglo oMyArreglo = new(10000);
+
+oMyArreglo.Llenar(5, 50);
+
+//oMyArreglo.ToString();
+
+Console.WriteLine(oMyArreglo);
+Console.ReadKey();
